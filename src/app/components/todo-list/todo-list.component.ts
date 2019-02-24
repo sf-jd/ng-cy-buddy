b8fc556e2de736466a21f121f80a110969e89b42
Santiago Fernandez <sfl.code@gmail.com>
2019-02-24 20:20:07 -0300

    Basic html structure and functionality created

---
+++ src/app/components/todo-list/todo-list.component.ts
@@ -0,0 +1,81 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../interfaces/todo';
+
+@Component({
+  selector: 'todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss']
+})
+export class TodoListComponent implements OnInit {
+
+  todos: Todo[];
+  todoTitle: string;
+  idForTodo: number;
+  beforeEditCache: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.idForTodo = 4;
+    this.todoTitle= '';
+    this.todos = [
+      {
+        'id': 1,
+        'title': 'First todo',
+        'completed': false,
+        'editing': false,
+      },
+      {
+        'id': 2,
+        'title': 'Second todo',
+        'completed': false,
+        'editing': false,
+      },
+      {
+        'id': 3,
+        'title': 'Third todo',
+        'completed': false,
+        'editing': false,
+      },
+    ]
+  }
+
+  addTodo(): void {
+
+    if(this.todoTitle.trim().length === 0){
+      return;
+    }
+
+    this.todos.push({
+      id: this.idForTodo,
+      title: this.todoTitle,
+      completed: false,
+      editing: false
+    })
+
+    this.todoTitle = '';
+    this.idForTodo++;
+  }
+
+  deleteTodo(id: number): void {
+    this.todos = this.todos.filter(todo => todo.id !== id);
+  }
+
+  editTodo(todo: Todo): void {
+    this.beforeEditCache = todo.title;
+    todo.editing = true;
+  }
+
+  cancelEdit(todo: Todo): void {
+    todo.title = this.beforeEditCache;
+    todo.editing = false;
+  }
+
+  doneEdit(todo: Todo): void {
+    if(todo.title.trim().length === 0){
+      todo.title = this.beforeEditCache;
+    }
+    todo.editing = false;
+  }
+
+}
