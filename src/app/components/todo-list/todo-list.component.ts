0f8ec600a2be9f8cd44ad7d1582f4f357bf2359e
Santiago Fernandez <sfl.code@gmail.com>
2019-02-24 20:34:57 -0300

    Add prettier cause this code deserves it

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -7,42 +7,40 @@ import { Todo } from '../../interfaces/todo';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
-
   todos: Todo[];
   todoTitle: string;
   idForTodo: number;
   beforeEditCache: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     this.idForTodo = 4;
-    this.todoTitle= '';
+    this.todoTitle = '';
     this.todos = [
       {
-        'id': 1,
-        'title': 'First todo',
-        'completed': false,
-        'editing': false,
+        id: 1,
+        title: 'First todo',
+        completed: false,
+        editing: false
       },
       {
-        'id': 2,
-        'title': 'Second todo',
-        'completed': false,
-        'editing': false,
+        id: 2,
+        title: 'Second todo',
+        completed: false,
+        editing: false
       },
       {
-        'id': 3,
-        'title': 'Third todo',
-        'completed': false,
-        'editing': false,
-      },
-    ]
+        id: 3,
+        title: 'Third todo',
+        completed: false,
+        editing: false
+      }
+    ];
   }
 
   addTodo(): void {
-
-    if(this.todoTitle.trim().length === 0){
+    if (this.todoTitle.trim().length === 0) {
       return;
     }
 
@@ -51,7 +49,7 @@ export class TodoListComponent implements OnInit {
       title: this.todoTitle,
       completed: false,
       editing: false
-    })
+    });
 
     this.todoTitle = '';
     this.idForTodo++;
@@ -72,10 +70,9 @@ export class TodoListComponent implements OnInit {
   }
 
   doneEdit(todo: Todo): void {
-    if(todo.title.trim().length === 0){
+    if (todo.title.trim().length === 0) {
       todo.title = this.beforeEditCache;
     }
     todo.editing = false;
   }
-
 }
