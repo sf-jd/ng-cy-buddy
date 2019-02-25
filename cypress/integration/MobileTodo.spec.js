7928a1629f4b11e7540a790d70fd956f25153669
Santiago Fernandez <sfl.code@gmail.com>
2019-02-25 00:17:57 -0300

    Update mobile spec file context name

--- cypress/integration/MobileTodo.spec.js
+++ cypress/integration/MobileTodo.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="Cypress" />
 
-context('ng Todo App', () => {
+context('ng Todo App Mobile resolution', () => {
   beforeEach(() => {
     cy.viewport('iphone-6');
     cy.visit('http://localhost:4200');
